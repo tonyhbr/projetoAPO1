239dc58aaabcc32435eddc4699ccf61ff721ec7d
Tony <Tony@DESKTOP-UONR96H>
2026-06-28 21:27:59 -0300

    a0.0.2
    patch: class VersaoTCC consertado o erro de versão de VersaoTCC

--- src/model/VersaoTCC.java
+++ src/model/VersaoTCC.java
@@ -3,17 +3,21 @@ package model;
 import java.time.LocalDate;
 
 public class VersaoTCC extends TCC {
-	protected Integer id;
-	protected String descricao;
-	protected LocalDate dataEnvio;
+	
+	private Integer id;
+	private String titulo;
+	private String status;
+	private LocalDate dataInicio;
+	private LocalDate dataEntrega;
+	private Aluno aluno;
+	private Orientador orientador;
+	private Coordenador coordenador;
+	private List<Termo> termos;
 	
 	public VersaoTCC(Integer id, String descricao, LocalDate dataEnvio) {
-		this.id = id;
-		this.descricao = descricao;
-		this.dataEnvio = dataEnvio;
+		super(id, descricao, dataEnvio);
+		// TODO Auto-generated constructor stub
 	}
-	
-	
 
 	public Integer getId() {
 		return id;
@@ -23,22 +27,68 @@ public class VersaoTCC extends TCC {
 		this.id = id;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
-	public LocalDate getDataEnvio() {
-		return dataEnvio;
+	public String getStatus() {
+		return status;
 	}
 
-	public void setDataEnvio(LocalDate dataEnvio) {
-		this.dataEnvio = dataEnvio;
+	public void setStatus(String status) {
+		this.status = status;
 	}
-	
-	
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public Orientador getOrientador() {
+		return orientador;
+	}
+
+	public void setOrientador(Orientador orientador) {
+		this.orientador = orientador;
+	}
+
+	public Coordenador getCoordenador() {
+		return coordenador;
+	}
+
+	public void setCoordenador(Coordenador coordenador) {
+		this.coordenador = coordenador;
+	}
+
+	public List<Termo> getTermos() {
+		return termos;
+	}
+
+	public void setTermos(List<Termo> termos) {
+		this.termos = termos;
+	}
 	
 }
